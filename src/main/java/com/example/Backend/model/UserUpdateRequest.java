556a1d19a1a0c46e5057397d0b02917d547c6753
여준 <duwns2057@naver.com>
2025-04-02 17:53:08 +0900

    서버

--- src/main/java/com/example/Backend/model/UserUpdateRequest.java
+++ src/main/java/com/example/Backend/model/UserUpdateRequest.java
@@ -1,14 +1,22 @@
 package com.example.Backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserUpdateRequest {
+
+    @JsonProperty("user_id")
     private String userId;
+
     private String password;
+
     private String nickname;
+
+    @JsonProperty("travel_destination")
     private String travelDestination;
+
     private String airline;
 }
