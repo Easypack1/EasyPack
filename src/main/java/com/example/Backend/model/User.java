160185db5d5f494061681332a71e81eb9fcb6ce1
여준 <duwns2057@naver.com>
2025-04-17 20:16:20 +0900

    서버

--- src/main/java/com/example/Backend/model/User.java
+++ src/main/java/com/example/Backend/model/User.java
@@ -24,7 +24,7 @@ public class User {
     @Column(nullable = false)
     private String nickname;
 
-    @Column(name = "travelDestination", nullable = false)
+    @Column(nullable = false)
     private String travelDestination;
 
     @Column(nullable = false)
